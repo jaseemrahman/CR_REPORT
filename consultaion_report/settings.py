3852ceb5a525bdef5f65e7e08e5a30823f0c08a3
akjal <jaleelak@gmail.com>
2022-07-24 21:08:42 +0530

    Update settings.py
    Added allowed hosts

--- consultaion_report/settings.py
+++ consultaion_report/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3gvf0zakt+q-5x57_jjk6ldix%cld=2*9&pb4x0c2ggxmjwr5z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['equipo-demo.azurewebsites.net','127.0.0.1']
+ALLOWED_HOSTS = ['equipo-demo.azurewebsites.net','127.0.0.1','equipo123.azurewebsites.net']
 
 
 # Application definition
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://equipo-demo.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://equipo-demo.azurewebsites.net","https://equipo123.azurewebsites.net"]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
